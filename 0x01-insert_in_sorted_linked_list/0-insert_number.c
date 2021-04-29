e83e66372606ef8d969dad234b089cbc8e1b50bf
nickarshadi <1477@holbertonschool.com>
2021-04-29 16:29:30 +0000

    Make code bettyy compatible.

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -1,58 +1,71 @@
+#include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "lists.h"
 
 /**
- * insert-node: Insert a number in a sorted singly linked list.
- * @head: Head of single linked list.
- * @number: Number to insert in linked list.
+ * create_node - creates a new node of type listint_t
+ * @number: value of the new node
+ *
+ * Return: the address of the new node
  */
+listint_t *create_node(int number)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	new->n = number;
+	new->next = NULL;
+
+	return (new);
+}
+
+
+/**
+ * insert_node - inserts a number into a sorted singly linked list
+ * @head: double pointer to the first node of the linked list
+ * @number: integer to insert
+ *
+ * Return: the address of the new node, or NULL if failed
+ */
+
 listint_t *insert_node(listint_t **head, int number)
 {
-  listint_t *new = NULL;
-  listint_t *node1 = NULL;
-  listint_t *node2 = NULL;
+	listint_t *cur;
+	listint_t *p;
+	listint_t *new;
 
-  if (head == NULL)
-    return (NULL);
-  new = malloc(sizeof(listint_t));
-  if (new == NULL)
-    return (NULL);
+	cur = *head;
+	p = *head;
+	new = create_node(number);
 
-  new->n = number;
-  new->next = NULL;
-  node1 = *head;
-  if (node1->next)
-  {
-    node2 = node1->next;
-  }
-  if (node2 == NULL)
-  {
-    add_nodeint_end(head, number);
-    free(new);
-    return (node1->next);
-  }
-
-  while (node1 != NULL)
-  {
-    if (node1->n <= number && node2->n >= number)
-  {
-    node1->next = new;
-    new->next = node2;
-    return (new);
-  }
-
-  if (node1->next == NULL)
-  {
-    add_nodeint_end(head, number);
-    free(new);
-    return (node1->next);
-  }
-    node1 = node1->next;
-    if (node1->next)
-      node2 = node1->next;
-    else
-      node2 = NULL;
-  }
-  return (NULL);
+	if (!head || !new)
+		return (NULL);
+	if (!*head)
+	{
+		*head = new;
+		return (new);
+	}
+	if (new->n < (*head)->n)
+	{
+		new->next = (*head);
+		*head = new;
+		return (new);
+	}
+	while (cur && *head)
+	{
+		if (cur->next == NULL)
+		{
+			cur->next = new;
+			new->next = NULL;
+		}
+		if (cur->n > new->n)
+		{
+			new->next = cur;
+			p->next = new;
+			break;
+		}
+		p = cur;
+		cur = cur->next;
+	}
+	return (new);
 }
