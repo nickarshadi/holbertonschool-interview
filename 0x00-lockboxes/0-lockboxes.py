2b43bb2f4cb8c84284abb4a2c6ed7a38dbed8cc3
nickarshadi <1477@holbertonschool.com>
2021-07-21 18:20:12 +0000

    Add basic functionality.

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Lockbox."""
+
+def canUnlockAll(boxes):
+    """Determine if all the boxes can be openedboxes is a list of lists.
+    Args:
+        boxes (list): a list of lists
+    Returns:
+        True if all boxes can be opened, else return FalseCheck if Boxes
+    """
+    return True
