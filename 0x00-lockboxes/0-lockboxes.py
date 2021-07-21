3596fa9d9d69dfa24a6553785eff0067399c8da5
nickarshadi <1477@holbertonschool.com>
2021-07-21 18:22:37 +0000

    Add logic.

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -9,4 +9,23 @@ def canUnlockAll(boxes):
     Returns:
         True if all boxes can be opened, else return FalseCheck if Boxes
     """
-    return True
+    stack = [0]
+    unlocked = [1] + [0] * (len(boxes) - 1)
+    i = 0
+
+    if len(boxes) == 0:
+        return True
+    if not isinstance(boxes, list):
+        return False
+    while stack:
+        p = stack.pop()
+        for key in boxes[p]:
+            if key > 0 and key < len(boxes) and unlocked[key] == 0:
+                unlocked[key] = 1
+                stack.append(key)
+        i = i + 1
+
+    if 0 in unlocked:
+        return False
+    else:
+        return True
